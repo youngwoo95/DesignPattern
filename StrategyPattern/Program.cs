9fbe9584e9ade7bcf0f247f68ca942b60c4b23d6
DESKTOP-551ENR9\kyw <rladyddn258@gmail.com>
2025-03-17 22:25:29 +0900

    전략패턴

--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -4,7 +4,77 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ShoppingCart cart = new ShoppingCart();
+
+            cart.setPaymentStrategy(new CreditCardPayment("John Doe", "1234567890123456"));
+            cart.checkout(100);
+
+            cart.setPaymentStrategy(new PayPalPayment("johndoe@example.com"));
+
+            cart.checkout(200);
         }
     }
+
+    // * [1] 인터페이스
+    interface PaymentStrategy
+    {
+        void pay(int amount);
+    }
+
+    // * [1] 인터페이스 구현
+    class CreditCardPayment : PaymentStrategy
+    {
+        private string name;
+        private string cardNumber;
+
+        public CreditCardPayment(string name, string cardNumber)
+        {
+            this.name = name;
+            this.cardNumber = cardNumber;
+        }
+
+        public void pay(int amount)
+        {
+            Console.WriteLine($"{amount} paid with credit card");
+        }
+    }
+
+    // * [1] 인터페이스 구현
+    class PayPalPayment : PaymentStrategy
+    {
+        private string email;
+        
+        public PayPalPayment(string email)
+        {
+            this.email = email;
+        }
+
+        public void pay(int amount)
+        {
+            Console.WriteLine($"{amount} paid using PayPal");
+        }
+    }
+
+    // Context Class
+    class ShoppingCart
+    {
+        // 이 자리에 해당 인터페이스를 적용한 두 클래스 중 하나가 들어갈 수 있다.
+        private PaymentStrategy paymentStrategy;
+
+        // 이 필드를 언제든 다른 전략으로 대체할 수 있도록 Setter 메서드도 있음.
+        public void setPaymentStrategy(PaymentStrategy paymentStrategy)
+        {
+            this.paymentStrategy = paymentStrategy;
+        }
+
+        // checkout 메서드를 실행하면 현재 장착된 전략 객체의 pay 메서드가 실행된다.
+        public void checkout(int amount)
+        {
+            paymentStrategy.pay(amount);
+        }
+    }
+
+
+
+
 }
---
+++ StrategyPattern2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+
+namespace StrategyPattern2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Compressor compressor = new Compressor();
+            string data = "aabcccccaaa";
+
+            compressor.setCompressionStrategy(new RunLengthEncoding());
+            Console.WriteLine($"RLE Compression {compressor.compress(data)}");
+
+            compressor.setCompressionStrategy(new SimpleReplacementCompression());
+            Console.WriteLine($"Simple Replacement {compressor.compress(data)}");
+        }
+    }
+
+    // * 인터페이스
+    interface CompressionStrategy
+    {
+        string compress(string data);
+    }
+
+    // * 인터페이스 를 구현하는 클래스
+    class RunLengthEncoding : CompressionStrategy
+    {
+        public string compress(string data)
+        {
+            StringBuilder compressed = new StringBuilder();
+            int count = 1;
+            for (int i = 1; i <= data.Length; i++)
+            {
+                if (i < data.Length && data[i] == data[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    compressed.Append(data[i - 1]);
+                    compressed.Append(count);
+                    count = 1;
+                }
+            }
+            return compressed.ToString();
+        }
+    }
+
+    // * 인터페이스 를 구현하는 클래스
+    class SimpleReplacementCompression : CompressionStrategy
+    {
+        public string compress(string data)
+        {
+            return data.Replace("a", "1")
+                   .Replace("e", "2")
+                   .Replace("i", "3")
+                   .Replace("o", "4")
+                   .Replace("u", "5");
+        }
+    }
+
+    // Context
+    class Compressor
+    {
+        private CompressionStrategy strategy;
+
+        public void setCompressionStrategy(CompressionStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public string compress(string data)
+        {
+            return strategy.compress(data);
+        }
+    }
+
+}
